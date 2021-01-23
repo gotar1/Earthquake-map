bd1209f2cf1ee78a0ca0751ec07c9aa3e5daf00d
Gotar <eltahir.g@gmail.com>
2021-01-23 04:38:41 -0600

    Final Revised Codes

--- Leaflet Earthquakes Maps/static/js/logic.js
+++ Leaflet Earthquakes Maps/static/js/logic.js
@@ -1,14 +1,16 @@
 // Store our API endpoint inside queryUrl
 let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
-// Perform a GET request to the query URL
+// Perform a GET request to the query URL to retrieve geoJSON data
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
   createFeatures(data.features);
   console.log(data.features)
 });
 
+// function to create features of each earthquake
 function createFeatures(earthquakeData) {
+  // function to add popup info about each earthquake
   function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + `Location: ${feature.properties.place}` + "</h3><hr><p>" +
      `<strong>Time</strong>: ${new Date(feature.properties.time)}` + "</p>" + "</h3><hr><p>" +
@@ -27,28 +29,28 @@ function createFeatures(earthquakeData) {
       let magnitude = feature.properties.mag;
 
       if (magnitude > 5) {
-        fillcolor = 'red';
+        fillcolor = '#ea2c2c';
       }
-      else if (magnitude >= 4) {
-        fillcolor = '#f0936b';
+      else if (magnitude > 4) {
+        fillcolor = '#ea822c';
       }
-      else if (magnitude >= 3) {
-        fillcolor = '#f3ba4e';
+      else if (magnitude > 3) {
+        fillcolor = '#ee9c00';
       }
-      else if (magnitude >= 2) {
-        fillcolor = '#f3db4c';
+      else if (magnitude > 2) {
+        fillcolor = '#eecc00';
       }
-      else if (magnitude >= 1) {
-        fillcolor = '#e1f34c';
+      else if (magnitude > 1) {
+        fillcolor = '#d4ee00';
       }
-      else  fillcolor = '#b7f34d';
+      else  fillcolor = '#98ee00';
 
       return L.circleMarker(latlng, {
         radius: radius,
         color: 'black',
         fillColor: fillcolor,
         fillOpacity: 1,
-        weight: 1
+        weight: 0.5
       });
     }
   });
@@ -57,6 +59,7 @@ function createFeatures(earthquakeData) {
   createMap(layerToMap);
 }
 
+// this is where we gonna build different tile layers maps
 function createMap(earthquakes) {
 
   // Define outdoormap layers
@@ -89,16 +92,34 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
+  // Here we make an AJAX call to get our Tectonic Plate geoJSON data.
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json",
+    function(platedata) {
+      // Adding our geoJSON data, along with style information, to the tectonicplates layer
+      L.geoJson(platedata, {
+        color: "orange",
+        weight: 2
+      })
+      .addTo(tectonicplates);
+
+      // Then add the tectonicplates layer to the map.
+      tectonicplates.addTo(myMap);
+  });
+
+  // We create the layers for the tectonicplates
+  let tectonicplates = new L.LayerGroup();
+
   // Define a baseMaps object to hold our base layers
   let baseMaps = {
-    "outdoor Map": outdoormap,
     "Dark Map": darkmap,
-    "satellite Map": satellitemap
+    "satellite Map": satellitemap,
+    "outdoor Map": outdoormap
   };
 
   // Create overlay object to hold our overlay layer
   let overlayMaps = {
-    Earthquakes: earthquakes
+    "Tectonic Plates": tectonicplates,
+    Earthquakes: earthquakes  
   };
 
   // Create our map, giving it the outdoormap and earthquakes layers to display on load
@@ -107,7 +128,7 @@ function createMap(earthquakes) {
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [outdoormap, earthquakes]
+    layers: [darkmap, satellitemap, outdoormap]
   });
 
   // Create a layer control
@@ -117,34 +138,40 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 
-   // function to assign colors for legend markers
-  function getColor(magnitude) {
-    return magnitude > 5 ? '#f06b6b':
-      magnitude > 4 ? '#f0936b':
-      magnitude > 3 ? '#f3ba4e':
-      magnitude > 2 ? '#f3db4c':
-      magnitude > 1 ? '#e1f34c':
-                      '#b7f34d';
-  }
-
   // position legend on map bottim left corner
-  let legend = L.control({position: 'bottomleft'});
+  let legend = L.control({position: "bottomright"});
 
+  // Then add all the details for the legend
   legend.onAdd = function() {
-    let div = L.DomUtil.create('div', 'legend');
-    let magnitudes = [0, 1, 2, 3, 4, 5];
-    // legendLabels = [];
+    let div = L.DomUtil.create("div", "info legend");
+
+    let grades = [0, 1, 2, 3, 4, 5];
+    let colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    // name the legend
     div.innerHTML = 'Magnitude<br><hr>' 
 
-    for (let i = 0; i < magnitudes.length; i++) {
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (let i = 0; i < grades.length; i++) {
       div.innerHTML +=
-      '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i>' + magnitudes[i] + (magnitudes[i + 1] ?
-      '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+        "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
     }
-    return div
+    return div;
   };
+
+  // Finally, we add our legend to the map.
   legend.addTo(myMap);
-}
+};
+
+
 
 
 
